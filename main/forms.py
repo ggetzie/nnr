b3475922a45b7c6b855e40f10f235c5f5865c248
gabe <ggetzie@gmail.com>
2019-12-21 08:25:48 -0500

    links to tos and privacy policy in form label

--- main/forms.py
+++ main/forms.py
@@ -10,6 +10,8 @@ from crispy_forms.layout import Layout, Submit, Fieldset, Div, HTML
 from django import forms
 from django.conf import settings
 from django.contrib.auth import get_user_model
+from django.urls import reverse_lazy
+from django.utils.safestring import mark_safe
 from django.utils.translation import ugettext_lazy as _
 from django.utils.text import slugify
 
@@ -22,21 +24,25 @@ import logging
 
 User = get_user_model()
 DUPE_MSG = _("A recipe with that title already exists!")
-TOS_LABEL = _("I have read and agree to the Terms of Service and "
-              "Privacy Policy")
 logger = logging.getLogger(__name__)
 
 class NNRSignupForm(SignupForm):
-    tos = forms.BooleanField(label=TOS_LABEL)
+    tos = forms.BooleanField()
         
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         today = datetime.date.today()
         paydate = today + relativedelta(days=30)
         payinfo = ("Your card will be charged an annual fee of $19 "
-                   "USD. The first charge will take place at the end of the 30 "
-                   f"day free trial period on {paydate} and thereafter "
+                   "USD. The first charge will take place at the end of the "
+                   f"30 day free trial period on {paydate} and thereafter "
                    "annually on that date.")
+        TOS_LINK = (f"""<a href="{reverse_lazy('tos')}" """ 
+                     """target="_blank">Terms of Service</a>""")
+        PP_LINK = (f"""<a href="{reverse_lazy('privacy')}" """ 
+                    """target="_blank">Privacy Policy</a>""")
+        self.fields["tos"].label = mark_safe(_("I have read and agree to the "
+                                               f"{TOS_LINK} and {PP_LINK}"))
                 
         self.helper = FormHelper()
         self.helper.form_id = "signup_form"
