a29bb0dca90963ccf2b5e2c109438ab840711dc2
gabe <ggetzie@gmail.com>
2020-03-11 11:12:35 +0800

    change from email. fix markdown help block quote

--- config/settings/production.py
+++ config/settings/production.py
@@ -140,7 +140,7 @@ TEMPLATES[0]["OPTIONS"]["loaders"] = [  # noqa F405
 # ------------------------------------------------------------------------------
 # https://docs.djangoproject.com/en/dev/ref/settings/#default-from-email
 DEFAULT_FROM_EMAIL = env(
-    "DJANGO_DEFAULT_FROM_EMAIL", default="No Nonsense Recipes <noreply@nononsense.recipes>"
+    "DJANGO_DEFAULT_FROM_EMAIL", default="No Nonsense Recipes <support@nononsense.recipes>"
 )
 # https://docs.djangoproject.com/en/dev/ref/settings/#server-email
 SERVER_EMAIL = "server@nononsense.recipes"
