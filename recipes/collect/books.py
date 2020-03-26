ae13468c53c72af8ab83b6a747d898b9a58d8b4b
gabe <ggetzie@gmail.com>
2020-03-26 08:08:15 +0800

    remove duplicate errors from signup form

--- recipes/collect/books.py
+++ recipes/collect/books.py
@@ -4,6 +4,6 @@ import pathlib
 CI_PATH = "/home/gabe/Dropbox/books/The Editors at America's Test Kitchen/The Cook's Illustrated Cookbook (657)"
 
 def ci_book(filepath):
-    with open(filepath)as book:
+    with open(filepath) as book:
         for line in book:
             pass
