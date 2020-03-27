e87e3c0a6587d2f4ddcee86da53f82b79002328c
gabe <ggetzie@gmail.com>
2020-03-27 10:34:05 +0800

    fixed superscript numbers in cibook

--- recipes/collect/books.py
+++ recipes/collect/books.py
@@ -1,7 +1,26 @@
 import json
 import pathlib
+import re
 
-CI_PATH = "/home/gabe/Dropbox/books/The Editors at America's Test Kitchen/The Cook's Illustrated Cookbook (657)"
+
+CI_PATH = pathlib.Path("/usr/local/src/nnr/recipes/collect/data/cibook.txt")
+
+weird_nums = {chr(178): "2",
+              chr(179): "3",
+              chr(185): "1"}
+
+def fix_weird_nums(book):
+    out=""
+    for i, c in enumerate(book):
+        if c in weird_nums:
+            if book[i-1].isdigit():
+                out += f"-{weird_nums[c]}"
+            else:
+                out += weird_nums[c]
+        else:
+            out += c
+    return out
+
 
 def ci_book(filepath):
     with open(filepath) as book:
