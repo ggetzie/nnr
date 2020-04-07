8e511679aa96e6020fc445553c14865e3fe5ad4c
gabe <ggetzie@gmail.com>
2020-04-07 11:07:44 +0800

    local only imports

--- imports.py
+++ imports.py
@@ -20,11 +20,11 @@ from nnr.users.models import *
 gabe = User.objects.get(username="gabe")
 admin = User.objects.get(username="admin")
 
-recipe_data = pathlib.Path("/usr/local/src/nnr/recipes/collect/data")
-from recipes.collect import books, ingest, scrape
-
 import requests
 import stripe
 
 stripe.api_key = settings.STRIPE_SK
 
+if settings.DEBUG:
+    recipe_data = pathlib.Path("/usr/local/src/nnr/recipes/collect/data")
+    from recipes.collect import books, ingest, scrape
