e7a2473e86dd25fb1b7c5740ab7609b1f68a7131
gabe <ggetzie@gmail.com>
2020-04-15 08:39:14 +0800

    fix instructions on paste

--- nnr/static/js/add.js
+++ nnr/static/js/add.js
@@ -11,3 +11,21 @@ document.getElementById("id_ingredients_text").addEventListener("paste", functio
         this.value += line.trim() + "  \n";
     }
 })
+
+document.getElementById("id_instructions_text").addEventListener("paste", function (event) {
+    // remove leading spaces and number lines
+    event.stopPropagation();
+    event.preventDefault();
+
+    let data = event.clipboardData || window.clipboardData;
+    let text = data.getData("text");
+    let i = 1;
+    for (line of text.split("\n")) {
+        if (line[0] === " ") {
+            this.value += `${i}. ${line.trim()}  \n`;
+            i += 1;
+        } else {
+            this.value += line + "\n";
+        }
+    }
+})
