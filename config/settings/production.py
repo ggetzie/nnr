b34185f52069b7db71123cdc384a7a0cc7513371
gabe <ggetzie@gmail.com>
2020-04-28 12:27:54 +0800

    save manifest?

--- config/settings/production.py
+++ config/settings/production.py
@@ -112,10 +112,31 @@ STATIC_URL = f"https://{AWS_S3_CUSTOM_DOMAIN}/static/"
 # Full-fledge class: https://stackoverflow.com/a/18046120/104731
 from storages.backends.s3boto3 import S3Boto3Storage  # noqa E402
 from django.contrib.staticfiles.storage import ManifestFilesMixin
+import pathlib
+import os
+import json
 
 class StaticRootS3Boto3Storage(ManifestFilesMixin, S3Boto3Storage):
     location = "static"
     default_acl = "public-read"
+
+    def read_manifest(self):
+        try:
+            with open(APPS_DIR.path(f"static/{self.manifest_name}")) as manifest:
+                return manifest.read().decode()
+        except FileNotFoundError:
+            return None
+
+    def save_manifest(self):
+        payload = {'paths': self.hashed_files, 
+                   'version': self.manifest_version}
+        manifest_path = pathlib.Path(APPS_DIR.path(f"static/{self.manifest_name}"))
+        if manifest_path.exists(): os.remove(manifest_path)
+        contents = json.dumps(payload).encode()
+        with open(manifest_path, "wb") as manifest_file:
+            manifest_file.write(contents)
+
+
 
 
 class MediaRootS3Boto3Storage(S3Boto3Storage):
