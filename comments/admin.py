9d6cbea8cdaca6ab64b7f494f9846cf09c37e117
gabe <ggetzie@gmail.com>
2021-08-10 09:11:42 +0800

    admin for recipes, tags, comments

--- comments/admin.py
+++ comments/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
+from comments.models import Comment
 
-# Register your models here.
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+
+    list_display = ["short_text", "user_name", "timestamp", "spam"]
+    list_editable = ["spam"]
+    exclude = ["html"]
+    ordering  = ["-timestamp"]
+
+    def short_text(self, obj):
+        return obj.text[:20]
+    short_text.short_description="Text"
+    
+    def user_name(self, obj):
+        return obj.user.username
+    user_name.short_description = "Username"
