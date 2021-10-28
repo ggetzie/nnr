91f639e18a2e74ae4d7ef3637935a50f39ef5384
gabe <ggetzie@gmail.com>
2021-10-28 21:51:53 +0800

    resize local photos

--- awslambda/photos/photos.go
+++ awslambda/photos/photos.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"os"
+	"path"
 
 	"github.com/h2non/bimg"
 )
@@ -17,16 +18,21 @@ func loadImageLocal(filepath string) (*bimg.Image, error) {
 
 func saveImageLocal(buffer []byte, filepath string) {
 	bimg.Write(filepath, buffer)
-
 }
 
 func getDefaultImageTypes() []bimg.ImageType {
 	return []bimg.ImageType{bimg.PNG, bimg.WEBP}
 }
 
-func getDefaultDims() [][]int {
+func getDefaultDims() map[int]bimg.ImageSize {
 	// Array of {width, height} to resize photos to
-	return [][]int{{1040, 585}, {860, 484}, {620, 349}, {490, 276}}
+	dims := map[int]bimg.ImageSize{
+		1200: {Width: 1090, Height: 818},
+		992:  {Width: 910, Height: 683},
+		768:  {Width: 670, Height: 503},
+		576:  {Width: 515, Height: 386},
+	}
+	return dims
 }
 
 func printMetadata(filepath string) {
@@ -34,14 +40,51 @@ func printMetadata(filepath string) {
 	if err != nil {
 		fmt.Fprintln(os.Stderr, err)
 	}
-
 	metadata, err := bimg.Metadata(buffer)
 	if err != nil {
 		fmt.Fprintln(os.Stderr, err)
 	}
-
 	fmt.Printf("metadata\n%+v", metadata)
+}
 
+func buildPath(folder string, width int, iType bimg.ImageType) string {
+	filename := fmt.Sprintf("%d.%v", width, bimg.ImageTypeName(iType))
+	return path.Join(folder, filename)
+}
+
+func resizeToHeight(originalDims bimg.ImageSize, height int) bimg.ImageSize {
+	newWidth := originalDims.Width * height / originalDims.Height
+	return bimg.ImageSize{Width: newWidth, Height: height}
+}
+
+func resizeToWidth(originalDims bimg.ImageSize, width int) bimg.ImageSize {
+	newHeight := originalDims.Height * width / originalDims.Width
+	return bimg.ImageSize{Width: width, Height: newHeight}
+}
+
+func smartDims(originalDims bimg.ImageSize, maxDims bimg.ImageSize) bimg.ImageSize {
+	// Calculate dims within max width and height, perserving aspect ratio
+	if (originalDims.Width <= maxDims.Width) && (originalDims.Height <= maxDims.Height) {
+		// already small enough
+		return originalDims
+	}
+	var resized bimg.ImageSize
+	if originalDims.Width > originalDims.Height {
+		// Landscape - more wide than tall
+		resized = resizeToWidth(originalDims, maxDims.Width)
+		if resized.Height > maxDims.Height {
+			// Width correct, but still too tall
+			resized = resizeToHeight(resized, maxDims.Height)
+		}
+	} else {
+		// Portrait - more tall than wide
+		resized = resizeToHeight(originalDims, maxDims.Height)
+		if resized.Width > maxDims.Width {
+			// height correct but still too wide
+			resized = resizeToWidth(resized, maxDims.Width)
+		}
+	}
+	return resized
 }
 
 func processImage(img *bimg.Image, save_to string) {
@@ -54,13 +97,29 @@ func processImage(img *bimg.Image, save_to string) {
 	if err != nil {
 		fmt.Fprintln(os.Stderr, err)
 	}
+	origDims, err := img.Size()
+	if err != nil {
+		fmt.Fprintln(os.Stderr, err)
+	}
+	origPath := path.Join(save_to, "orig.jpeg")
+	saveImageLocal(origImage, origPath)
 
 	imageTypes := getDefaultImageTypes()
 	dims := getDefaultDims()
 
-	// metadata, err := bimg.Metadata(newImage)
-	// if err != nil {
-	// 	fmt.Fprintln(os.Stderr, err)
-	// }
-
+	for screenSize, dim := range dims {
+		for _, iType := range imageTypes {
+			savePath := buildPath(save_to, screenSize, iType)
+			newDims := smartDims(origDims, dim)
+			newImage, err := bimg.NewImage(origImage).Resize(newDims.Width, newDims.Height)
+			if err != nil {
+				fmt.Fprintln(os.Stderr, err)
+			}
+			newImage, err = bimg.NewImage(newImage).Convert(iType)
+			if err != nil {
+				fmt.Fprintln(os.Stderr, err)
+			}
+			saveImageLocal(newImage, savePath)
+		}
+	}
 }
--- awslambda/photos/photos_test.go
+++ awslambda/photos/photos_test.go
@@ -1,7 +1,47 @@
 package main
 
-import "testing"
+import (
+	"errors"
+	"fmt"
+	"os"
+	"path"
+	"testing"
+
+	"github.com/h2non/bimg"
+)
 
 func TestPrintMetadata(t *testing.T) {
 	printMetadata("/media/gabe/data/pictures/websites/nnr/test/1/orig.jpg")
+}
+
+func TestBuildPath(t *testing.T) {
+	want := "/media/images/recipes/3/1040.jpeg"
+	result := buildPath("/media/images/recipes/3", 1040, bimg.JPEG)
+	if result != want {
+		t.Fatalf(`buildPath Failed. Wanted %q Got %v`, want, result)
+	}
+}
+
+func TestProcessImage(t *testing.T) {
+	folder := "/media/gabe/data/pictures/websites/nnr/test/1"
+	file := "orig.jpeg"
+	filepath := path.Join(folder, file)
+	img, err := loadImageLocal(filepath)
+	if err != nil {
+		t.Fatalf(`failed to load test image: %v`, filepath)
+	}
+	processImage(img, folder)
+	iTypes := getDefaultImageTypes()
+	dims := getDefaultDims()
+	for screenSize := range dims {
+		for _, iType := range iTypes {
+			filename := fmt.Sprintf("%d.%s", screenSize, bimg.ImageTypeName(iType))
+			filepath := path.Join(folder, filename)
+			_, err := os.Stat(filepath)
+			if errors.Is(err, os.ErrNotExist) {
+				t.Fatalf(`Could not find output: %v`, filepath)
+			}
+		}
+
+	}
 }
