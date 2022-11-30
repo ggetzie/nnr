3de78fe1bcb0fed17133f252a3ad0f00dd32040b
Gabriel Getzie <gabe@kotsf.com>
2022-11-30 10:05:42 -0500

    run go rotd locally

--- recipes/management/rotd.sh
+++ recipes/management/rotd.sh
@@ -2,4 +2,7 @@
 
 cd /usr/local/src/nnr
 source /usr/local/src/env/nnr/bin/activate
+source $HOME/bin/export_dotenv nnr
+# use local option
+/usr/local/src/nnr/awslambda/rotd/build/rotd --test
 ./manage.py choose_rotd
