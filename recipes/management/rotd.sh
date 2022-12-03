5d9f049b344f3c2e1c5be1b5936b043a2072ec8c
Gabriel Getzie <gabe@kotsf.com>
2022-12-03 08:49:33 -0500

    better comments

--- recipes/management/rotd.sh
+++ recipes/management/rotd.sh
@@ -2,7 +2,8 @@
 
 cd /usr/local/src/nnr
 source /usr/local/src/env/nnr/bin/activate
+# load environment variables from .env file
 source $HOME/bin/export_dotenv nnr
-# use local option
+# use --test flag to run locally from command line
 /usr/local/src/nnr/awslambda/rotd/build/rotd --test
 ./manage.py choose_rotd
