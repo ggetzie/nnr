e45e8c40a1773764a891ec7aa5d9f61698a685f8
Gabriel Getzie <gabe@kotsf.com>
2024-03-09 17:28:52 +0800

    fix twitter posting, TODO facebook posting

--- recipes/management/commands/choose_rotd.py
+++ recipes/management/commands/choose_rotd.py
@@ -1,11 +1,29 @@
-
+import datetime
 import logging
 
+from django.conf import settings
 from django.core.cache import cache
 from django.core.cache.utils import make_template_fragment_key
 from django.core.management.base import BaseCommand
 
+from recipes.models import Recipe
+from recipes.twitter import create_tweet
+
 logger = logging.getLogger(__name__)
+
+
+def set_new_rotd() -> Recipe:
+    today = datetime.date.today()
+    omago = today - datetime.timedelta(days=30)
+    old_rotd = Recipe.objects.get(featured=True)
+    new_rotd = Recipe.objects.filter(last_featured__lt=omago).order_by("?").first()
+    old_rotd.featured = False
+    new_rotd.featured = True
+    new_rotd.last_featured = today
+    old_rotd.save()
+    new_rotd.save()
+    return new_rotd
+
 
 class Command(BaseCommand):
 
@@ -16,6 +34,19 @@ class Command(BaseCommand):
         This command is kept to be run at the same time (or shortly after) only to
         delete the cache entry
         """
-        
+
         cache_key = make_template_fragment_key("rotd")
         cache.delete(cache_key)
+
+        new_rotd = set_new_rotd()
+
+        # post to twitter
+        create_tweet(
+            settings.TWITTER_CONSUMER_KEY,
+            settings.TWITTER_CONSUMER_SECRET,
+            settings.TWITTER_ACCESS_TOKEN,
+            settings.TWITTER_ACCESS_SECRET,
+            new_rotd.as_tweet(),
+        )
+
+        # TODO fix posting to facebook when approved
